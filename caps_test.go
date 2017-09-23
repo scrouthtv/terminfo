63efac70f478f5c6f2d306925454642bc16dfe82
Kenneth Shaw <kenneth.shaw@knq.io>
2017-09-23 07:33:29 +0700

    Adding cap names tests

--- caps_test.go
+++ caps_test.go
@@ -15,3 +15,42 @@ func TestCapSizes(t *testing.T) {
 		t.Fatalf("stringCapNames should have same length as twice stringCapCount")
 	}
 }
+
+func TestCapNames(t *testing.T) {
+	for i := BoolCapType(0); i < BoolCapType(capCountBool); i++ {
+		n, s := BoolCapName(i), BoolCapNameShort(i)
+		if n == "" {
+			t.Errorf("Bool cap %d should have name", i)
+		}
+		if s == "" {
+			t.Errorf("Bool cap %d should have short name", i)
+		}
+		if n == s {
+			t.Errorf("Bool cap %d name and short name should not equal (%s==%s)", i, n, s)
+		}
+	}
+	for i := NumCapType(0); i < NumCapType(capCountNum); i++ {
+		n, s := NumCapName(i), NumCapNameShort(i)
+		if n == "" {
+			t.Errorf("Num cap %d should have name", i)
+		}
+		if s == "" {
+			t.Errorf("Num cap %d should have short name", i)
+		}
+		if n == s && n != "lines" {
+			t.Errorf("Num cap %d name and short name should not equal (%s==%s)", i, n, s)
+		}
+	}
+	for i := StringCapType(0); i < StringCapType(capCountString); i++ {
+		n, s := StringCapName(i), StringCapNameShort(i)
+		if n == "" {
+			t.Errorf("String cap %d should have name", i)
+		}
+		if s == "" {
+			t.Errorf("String cap %d should have short name", i)
+		}
+		if n == s && n != "tone" && n != "pulse" {
+			t.Errorf("String cap %d name and short name should not equal (%s==%s)", i, n, s)
+		}
+	}
+}
