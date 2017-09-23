a4dac66946def41ffd0d09f1c03b2c67a500ce0c
Kenneth Shaw <kenneth.shaw@knq.io>
2017-09-23 07:17:08 +0700

    Updating Open test

--- terminfo_test.go
+++ terminfo_test.go
@@ -1,33 +1,49 @@
 package terminfo
 
 import (
+	"os"
+	"path/filepath"
+	"regexp"
 	"testing"
 )
 
-// TODO look at unibillium tests
-func TestOpen(t *testing.T) {
-	ti, err := LoadFromEnv()
-	if err != nil {
-		t.Fatal(err)
-	}
-	ti = ti
-	//t.Logf("%q", ti.ExtStrings["kUP7"])
-	//t.Logf("%q", ti.Strings[FlashScreen])
-	//	b := bytes.NewBuffer(nil)
-	//	ti.Strings[PadChar] = "*"
-	//ti.Puts(b, ti.Strings[FlashScreen], 1, 9600)
-	//	t.Logf("%q", b.Bytes())
-	//t.Logf("%q", ti.Color(1, 1))
-}
+var fileRE = regexp.MustCompile("^([0-9]+|[a-zA-Z])/")
 
-/*func BenchmarkParm(b *testing.B) {
-	ti, err := LoadFromEnv()
-	if err != nil {
-		b.Fatal(err)
+func TestOpen(t *testing.T) {
+	for _, dir := range []string{"/lib/terminfo", "/usr/share/terminfo"} {
+		t.Run(dir[1:], func(dir string) func(*testing.T) {
+			return func(t *testing.T) {
+				t.Parallel()
+				t.Logf("processing dir %s", dir)
+				filepath.Walk(dir, func(file string, fi os.FileInfo, err error) error {
+					if fi.IsDir() || !fileRE.MatchString(file[len(dir)+1:]) {
+						t.Logf("skipping: %s", file)
+						return nil
+					}
+
+					term := filepath.Base(file)
+
+					t.Logf("opening %s (%s)", file, term)
+
+					// open
+					ti, err := Open(dir, term)
+					if err != nil {
+						t.Fatalf("term %s expected no error, got: %v", term, err)
+					}
+
+					if ti.File != file {
+						t.Errorf("term %s should have file %s, got: %s", term, file, ti.File)
+					}
+
+					// check we have at least one name
+					if len(ti.Names) < 1 {
+						t.Errorf("term %s expected names to have at least one value", term)
+					}
+					t.Logf("term %s -- '%s'", term, ti.Names[len(ti.Names)-1])
+
+					return nil
+				})
+			}
+		}(dir))
 	}
-	var r string
-	for i := 0; i < b.N; i++ {
-		r = ti.Color(7, 5)
-	}
-	result = r
-}*/
+}
